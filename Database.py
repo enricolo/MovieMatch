3892bf02afa60e72ae41083675e27c341fcf578e
enricolo <enricolonardi@gmail.com>
2021-03-02 02:38:16 +0100

    changed database drastically to implement personalized movie lists

--- Database.py
+++ Database.py
@@ -1,4 +1,5 @@
 import sqlite3   
+import time
  
 def absolutePath():
     return "/home/enricolo/Documents/VisualStudioCode/PlexApiTest/"
@@ -16,22 +17,20 @@ def create_catalogo():
     genre TEXT,   
     audienceRating REAL,
     studio TEXT,
-    plot TEXT
+    plot TEXT, 
+    imdbid TEXT
     )""")
 
 
-def insert_data(title, titleIta, year, image, genre, audienceRating, studio, plot):
+def insert_data(title, titleIta, year, image, genre, audienceRating, studio, plot, imdbid):
     conn = sqlite3.connect(absolutePath()+'database.db')
     c = conn.cursor()
     with conn:
-        # try:
-            print(">>>>>>>>>NEW ELEMENT")
-            #element is new, 
-            c.execute("""INSERT INTO catalogo VALUES (:title, :titleIta, :year, :image, :genre, :audienceRating, :studio, :plot)""", 
-            {'title':title,'titleIta':titleIta, 'year':year, 'image':image, 'genre':genre, 'audienceRating':audienceRating,'studio':studio, 'plot':plot})
-        # except sqlite3.IntegrityError as e:
-        #     pass
 
+        print(">>>>>>>>>NEW ELEMENT")
+        #element is new, 
+        c.execute("""INSERT INTO catalogo VALUES (:title, :titleIta, :year, :image, :genre, :audienceRating, :studio, :plot, :imdbid)""", 
+        {'title':title,'titleIta':titleIta, 'year':year, 'image':image, 'genre':genre, 'audienceRating':audienceRating,'studio':studio, 'plot':plot, 'imdbid':imdbid})
 
 
 def get_alldata():
@@ -49,7 +48,7 @@ def get_alltitle():
 def get_top():
     conn = sqlite3.connect(absolutePath()+'database.db')
     c = conn.cursor()
-    c.execute("""SELECT title, image, year FROM catalogo ORDER BY random() LIMIT 1""")
+    c.execute("""SELECT title, image, year FROM catalogo ORDER BY random() LIMIT 1""") #
     return c.fetchall()
 
 
@@ -68,15 +67,61 @@ def update_info(title, image, genre, plot, studio):
         c.execute("""UPDATE catalogo SET image=:image, genre=:genre, plot=:plot, studio=:studio WHERE title = :title COLLATE NOCASE""",
             {'title':title, 'image':image, 'genre':genre, 'plot':plot, 'studio':studio})
 
+##############################################################################
+#                             PLATFORM TABLE
+##############################################################################   
+
+
+def create_platform_catalogo():
+    conn = sqlite3.connect(absolutePath()+'database.db')
+
+    c = conn.cursor()
+    #da testare unicità chiave (url + name)
+    c.execute("""CREATE TABLE IF NOT EXISTS platforms (
+    title TEXT NOT NULL COLLATE NOCASE,
+    year INTEGER NOT NULL, 
+    imdbid TEXT,
+    platform TEXT
+    )""")
+    
+def insert_film(title, year,imdbid, platform):
+    conn = sqlite3.connect(absolutePath()+'database.db')
+    c = conn.cursor()
+    with conn:
+        # print(title)
+        c.execute("""INSERT OR REPLACE INTO platforms VALUES (:title, :year, :imdbid, :platform)""", 
+        {'title':title,'year':year,'imdbid':imdbid,'platform':platform})
+ 
+def check_if_new(title, year,imdbid):
+    conn = sqlite3.connect(absolutePath()+'database.db')
+    c = conn.cursor()        
+    c.execute("""SELECT * 
+        FROM platforms as p 
+        WHERE exist (
+            SELECT 1 
+            FROM catalogo as c
+            WHERE c.title = p.title AND c.year = p.year AND c.title = :title AND c.year = :year)""", 
+        {'title':title,'year':year,'imdbid':imdbid})
+    
+    return c.fetchall()
+
+def removeplatforms():
+    conn = sqlite3.connect(absolutePath()+'database.db')
+    c = conn.cursor()
+    with conn:
+        c.execute("""DELETE from platforms""")
+                
+def drop_platforms():
+    conn = sqlite3.connect(absolutePath()+'database.db')
+    conn.execute("DROP TABLE platforms")
+    conn.commit()            
+        
 
 ##############################################################################
 #                             USER TABLE
 ##############################################################################
 
-def drop_users():
-    conn = sqlite3.connect(absolutePath()+'database.db')
-    conn.execute("DROP TABLE users")
-    conn.commit()
+
     
 def user_catalogo():
     conn = sqlite3.connect(absolutePath()+'database.db')
@@ -94,8 +139,8 @@ def insert_user(user): #TODO 2 concurrent username might be a problem
     with conn:
         
         #element is new, 
-        c.execute("""INSERT OR REPLACE INTO users VALUES (:user , :title, :year)""", 
-        {'user':user, 'title':'', 'year':''})
+        c.execute("""INSERT OR REPLACE INTO users VALUES (:user , :password, :altro)""", 
+        {'user':user, 'password':'', 'altro':''})
         
 
 def get_user(user):
@@ -112,6 +157,10 @@ def removeuser(user):
     with conn:
         c.execute("""DELETE from catalogo WHERE user = :user""")
         
+def drop_users():
+    conn = sqlite3.connect(absolutePath()+'database.db')
+    conn.execute("DROP TABLE users")
+    conn.commit()
         
 ##############################################################################
 #                             approved_list
@@ -146,7 +195,7 @@ def get_userfilms(user):
     return c.fetchall()
 
 
-def get_find_match(user1,user2):
+def get_find_match(user1,user2):#TODO solve problem with films with same title
     conn = sqlite3.connect(absolutePath()+'database.db')
     c = conn.cursor()
     c.execute("""SELECT *
@@ -176,5 +225,183 @@ def remove_approved_film(user, title):
         c.execute("""DELETE from approvedmovies WHERE user = :user and title = :title""",
             {'user':user, 'title':title})
     
+##############################################################################
+#                             loading_table
+##############################################################################   
+
+def create_settings():
+    conn = sqlite3.connect(absolutePath()+'database.db')
+    c = conn.cursor()
     
+    c.execute("""CREATE TABLE IF NOT EXISTS settings (
+    name_table TEXT,
+    platform_common TEXT,
+    genres_common TEXT,
+    setoffset INTEGER,
+    setlimit INTEGER,
+    creationdate INTEGER
+    )""")
+    
+
+#FIRST insertion of information of the first user
+def insert_setting(user1, user2, platforms, genres):
+    if get_settings(user1, user2) is not None:
+        change_settings(user1, user2, platforms, genres)
+    else:
+        userA,userB = sortusers(user1, user2)
+        conn = sqlite3.connect(absolutePath()+'database.db')
+        c = conn.cursor()
+        with conn:
+            
+            c.execute("""INSERT INTO settings (name_table, platform_common, genres_common) 
+                    VALUES (:name_table, :platform_common , :genres)""", 
+            {'name_table':userA+userB,'platform_common':platforms, 'genres':genres})
+    
+        
+
+def change_settings(user1, user2, platforms, genres):
+    res = get_settings(user1, user2)
+    print(res)
+    platform_common = set(res[1].split(", "))
+    genres_common = set(res[2].split(", "))
+    platforms = set(platforms.split(", "))
+    genres = set(genres.split(", "))
+    
+    platforms = platform_common & platforms
+    genres = genres_common.union(genres) 
+    
+    platform_common = ""
+    genres_common = ""
+    
+    for p in platforms:
+        platform_common += p + ", "
+    for g in genres:
+        genres_common += g + ", "
+    
+    userA,userB = sortusers(user1, user2)
+    
+    conn = sqlite3.connect(absolutePath()+'database.db')
+    c = conn.cursor()
+    with conn:
+        c.execute("UPDATE settings SET platform_common = :platform_common, genres_common = :genres_common, setoffset = :setoffset, setlimit = :setlimit, creationdate = :creationdate WHERE name_table = :name_table", 
+        {'name_table':userA+userB,'platform_common':platform_common, 'genres_common':genres_common, 'setoffset':0, 'setlimit':100, 'creationdate':int(time.time()/3600)})
+
+
+def get_settings(user1, user2):
+    userA,userB = sortusers(user1, user2)
+    
+    conn = sqlite3.connect(absolutePath()+'database.db')
+    c = conn.cursor()
+    c.execute("""SELECT * FROM settings WHERE name_table = :name_table""", 
+        {'name_table':userA+userB})
+    return c.fetchone()
+
+def is_row_complete(user1, user2):
+    userA,userB = sortusers(user1, user2)
+    conn = sqlite3.connect(absolutePath()+'database.db')
+    c = conn.cursor()
+    
+    c.execute("""SELECT 1 FROM settings WHERE name_table = :name_table""", 
+    {'name_table':userA+userB})
+    
+    if c.fetchone():
+        create_users_table(user1, user2)
+        return 1
+    else:
+        return None
+
+
+def drop_loading_list(user1, user2):
+    conn = sqlite3.connect(absolutePath()+'database.db')
+    conn.execute("DROP TABLE settings")
+    conn.commit()
+
+############################################################################## testo += ""
+#                             User movie table
+##############################################################################   
+
+def create_users_table(user1, user2):
+    userA,userB = sortusers(user1, user2)
+    conn = sqlite3.connect(absolutePath()+'database.db')
+
+    testo = "CREATE TABLE IF NOT EXISTS "+userA+userB+" ("
+    testo += "title TEXT NOT NULL COLLATE NOCASE,"
+    testo += "year INTEGER NOT NULL, "
+    testo += "imdbid TEXT, "
+    testo += userA + " TEXT, "
+    testo += userB + " TEXT)"    
+
+    c = conn.cursor()
+    c.execute(testo)
+
+def populate_user_movies(user1, user2): #TODO doesn't consider streaming platform
+    userA,userB = sortusers(user1, user2)
+    res = get_settings(user1, user2)
+    
+    platform = res[1].split(", ")
+    genres = res[2].split(", ")
+    offset = res[3]
+    
+    
+    testo ="INSERT INTO "+userA+userB + " (title, year, imdbid) " 
+    testo += "SELECT title, year, \"\" " 
+    testo += "FROM catalogo "
+    testo += "WHERE "
+    for genre in genres:
+        if genre == "":
+            continue
+        testo += "genre not like \"%"+genre+"%\" AND " 
+    testo += "genre != \"\" "
+    testo += "ORDER BY audienceRating DESC Limit 100 OFFSET "+str(offset)
+    
+    print (testo)
+    
+    conn = sqlite3.connect(absolutePath()+'database.db')
+    c = conn.cursor()
+    with conn:
+        c.execute(testo)
+    
+    
+def set_user_approved(user1, user2, title):
+    userA,userB = sortusers(user1, user2)
+    
+    conn = sqlite3.connect(absolutePath()+'database.db')
+    c = conn.cursor()
+    with conn:
+        c.execute("UPDATE "+userA+userB+" SET "+user1+"="+str(1)+" WHERE title = \""+title+"\" COLLATE NOCASE")
+        
+#return a random movie from the movie list of the user       
+def get_user_film(user1, user2): 
+    userA,userB = sortusers(user1, user2)
+    
+    conn = sqlite3.connect(absolutePath()+'database.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM catalogo as c WHERE c.title in (SELECT A.title FROM "+userA+userB+" as A WHERE "+user1+" IS NULL ORDER BY random() LIMIT 1 )")
+    res = c.fetchone()
+    if res is not None:
+        print(res[0])
+        with conn:
+            c.execute("UPDATE "+userA+userB+" SET "+user1+"=\"?\" WHERE title = \""+res[0]+"\"")
+        return res
+    else:
+        populate_user_movies(user1, user2)
+        #TODO change offset by 100
+
+
+
+def drop_user_movie_table(user1, user2):
+    userA,userB = sortusers(user1, user2)
+    conn = sqlite3.connect(absolutePath()+'database.db')
+    conn.execute("DROP TABLE "+userA+userB)
+    conn.commit()
+
+##############################################################################
+#                             sortmethod
+##############################################################################   
+
+def sortusers(user1, user2):
+    if(user1 < user2):
+        return user1, user2
+    
+    return user2, user1
     
--- Server.py
+++ Server.py
@@ -16,11 +16,11 @@ approvedFilmsArgs.add_argument("movie", type=str, help="name of movie", required
 
 
 usernameArgs = reqparse.RequestParser()
-usernameArgs.add_argument("id1", type=str, required=True)
+usernameArgs.add_argument("id1", help="name of user", type=str, required=True)
 
 username2Args = reqparse.RequestParser()
-username2Args.add_argument("id1", type=str, required=True)
-username2Args.add_argument("id2", type=str, required=True)
+username2Args.add_argument("id1", help="name of user", type=str, required=True)
+username2Args.add_argument("id2", help="name of user2", type=str, required=True)
 
 
 #implement offset
@@ -61,8 +61,10 @@ class checkFilms(Resource):
                 
         response = []
         if commonmovies:
+            i = 0
             for movie in commonmovies:
                 response.append({
+                    "res":              i,
                     "Title":            movie[0],
                     "TitleIta":         movie[1],
                     "Year":             movie[2],
@@ -72,12 +74,13 @@ class checkFilms(Resource):
                     "Studio":           movie[6],
                     "Plot":             movie[7]
                     })
-            Database.remove_user_list(args['id1'])
-            Database.remove_user_list(args['id2'])
-            return {"res" : response}, 200
+                i+=1
+            # Database.remove_user_list(args['id1'])
+            # Database.remove_user_list(args['id2'])
+            return response, 200
         
         else:
-            return {"res" : "false"}, 404
+            return [{"res" : "false"}], 200
         
 
             
@@ -85,6 +88,7 @@ class searchUser(Resource):
     def get(self): 
         args = username2Args.parse_args()
         print(args)
+
         if(args['id1'] == args['id2']):   #check if not null
             return {"res" : "same name"}, 404
         user = Database.get_user(args['id2'])
@@ -92,14 +96,15 @@ class searchUser(Resource):
         if user:
             return {"res" : "found"}, 200
         else:
-            return {"res" : "not exists"}, 404
-        # return {"res" : "found"}, 200
+            return {"res" : "not exists"}, 200
         
     
     
 class userLogin(Resource):
     def post(self):
         args = usernameArgs.parse_args()
+        if len(args['id1']) == 0:
+            return {"res" : "bad name"}, 400
         print(args)
         print(args['id1'])
         
@@ -118,5 +123,4 @@ api.add_resource(checkFilms, "/checkFilms/")
         
 
 if __name__ == "__main__":
-    # app.run(debug=True,port=10010) #need to call localhost 
     app.run(debug=True, host= '0.0.0.0',port=10010) #need to call serverip      if timeout reached check serverip
